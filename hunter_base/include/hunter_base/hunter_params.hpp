f9d50788929ac530d1e02c98f8bb1dafcb16ae36
Steven Brills <sbrills@oshkoshcorp.com>
2026-09-18 13:03:20 -0400

    fix: merge hunter_se params into the hunter-integration branch, update vehicle params with measured stats ADV-22

--- hunter_base/include/hunter_base/hunter_params.hpp
+++ hunter_base/include/hunter_base/hunter_params.hpp
@@ -33,18 +33,18 @@ struct HunterV2Params {
 
 struct HunterSEParams {
   static constexpr double track =
-      0.55;  // in meter (left & right wheel distance)
+      0.52; //0.55;  // in meter (left & right wheel distance)
   static constexpr double wheelbase =
-      0.52;  // in meter (front & rear wheel distance)
+      0.55; //0.52;  // in meter (front & rear wheel distance)
   static constexpr double wheel_radius = 0.1375;              // in meter
   static constexpr double transmission_reduction_rate = 30;  // 1:30
 
   // from user manual v1.2.6_S P4
   // max linear velocity: 1.5 m/s
   static constexpr double max_steer_angle =
-      0.283;  // in rad, 0.75 for inner wheel
+      0.45278; //0.283;  // in rad, 0.75 for inner wheel
   static constexpr double max_steer_angle_central =
-      0.384;  // max central angle
+      0.377; //0.384;  // max central angle
   static constexpr double max_linear_speed = 1.5;  // in m/ss
 };
 
